619322f4f1271968090238869683fafdd71ff553
Balaji Patnam <balubunny4u@gmail.com>
2021-10-09 21:52:47 +0530

    Implemented material

--- skeleton-material/src/components/main/Dashboard/index.js
+++ skeleton-material/src/components/main/Dashboard/index.js
@@ -1,13 +1,24 @@
-import React, { useEffect } from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Header from "components/layouts/Header";
 
-import "./dashboard.scss";
-
-function Dashboard() {
-  useEffect(init, []);
-
-  function init() {}
-
-  return <section className="dashboard">dadh</section>;
+export default function Dashboard() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <Box sx={{ my: 2 }}>
+          {[...new Array(100)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+            )
+            .join("\n")}
+        </Box>
+      </Container>
+    </>
+  );
 }
-
-export default Dashboard;
