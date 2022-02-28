f01a47917098688049c6f975e8a41ca8dc79afbb
Balaji Patnam <balubunny4u@gmail.com>
2022-02-28 14:37:30 +0530

    Added configuration for next js scss global variable

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,6 +1,11 @@
 const path = require("path");
 
 module.exports = {
+  sassOptions: {
+    // Access global scss variables into CSS/Scss Modules
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import "assets/styles/partials/variables.scss";`,
+  },
   images: {
     domains: ["cdn.pixabay.com"],
   },
